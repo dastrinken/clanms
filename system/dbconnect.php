e511c61bf348db581354c688f34027a9b444a17a
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-02-05 14:06:59 +0100

    Navbar test

--- system/dbconnect.php
+++ system/dbconnect.php
@@ -3,7 +3,5 @@
     $mysqli = new mysqli($dbhost, $dbuser, $dbpw, $db);
     if($mysqli->connect_error) {
         die("Connection to Database failed..." . $mysqli->connect_error);
-    } else {
-        echo "Connection to Database established!";
     }
 ?>
