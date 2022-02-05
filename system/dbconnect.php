8c058937cff3bf5d4da84d8f3f30cb2cb3d7743c
WackyPete <silas.waldschmidt@iem.thm.de>
2022-02-05 14:01:14 +0100

    4. test

--- system/dbconnect.php
+++ system/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-    require(".system/dbsettings.php");
+    require("./system/dbsettings.php");
     $mysqli = new mysqli($dbhost, $dbuser, $dbpw, $db);
     if($mysqli->connect_error) {
         die("Connection to Database failed..." . $mysqli->connect_error);
