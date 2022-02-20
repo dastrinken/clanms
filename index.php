ae2e54113dfc8ea9b2e47ba3240de57be9c3d845
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-02-20 19:56:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- CSS only -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./bootstrap/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="./styles/style.css"> 
     <script src="./bootstrap/js/bootstrap.bundle.js"></script>
