7a148e198425bc83c5d349fd286ed2d1d1cebe75
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-02-25 12:04:39 +0100

    Hinweise und Ideen

---
+++ install/index.php
@@ -0,0 +1,10 @@
+<?php
+/* Ideen:
+** 1. Siehe Ablaufmodellierung
+** 2. Diese Datei soll die erste sein, die der Nutzer nach Upload des gesamten Verzeichnisses sieht!
+**    - Dazu soll sie zu Beginn im "Hauptordner" liegen und die eigentliche index.php ersetzen
+** 3. Diese Datei soll nach gelungener Installation automatisch "inaktiviert" und in den Ordner "install" verschoben werden.
+
+
+*/
+?>
