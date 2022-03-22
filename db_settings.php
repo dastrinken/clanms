c732f9407ad6b7f975dd5f80bdbc436aa2c69ab8
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-03-22 20:06:42 +0100

    Delete db_settings.php

--- db_settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /* TODO: File has to be written during automatic installation process. 
-    In a real release, there should be no need to manually change the values */
-    $dbpre = "clanms_";
-    $dbhost = "localhost";
-    $dbpw = "IZMDPMrIxp6oT7QnqWKA";
-    $dbuser = "clanms";
-    $db = "clanms";
-?>
