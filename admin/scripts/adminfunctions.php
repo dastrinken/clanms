5d743584f8785fd58e5355a24dcfbb3f1e0a4166
WackyPete <silas.waldschmidt@iem.thm.de>
2022-03-29 19:54:58 +0200

    Erweiterung Funktionalität AdminDashboard

--- admin/scripts/adminfunctions.php
+++ admin/scripts/adminfunctions.php
@@ -46,4 +46,10 @@
     if($_GET['deleteGallery'] === 'true') {
         deleteGalleryFromDB($_GET['galleryId']);
     }
+    
+    /*Game */
+    if($_POST['deleteGame'] === 'true'){
+        var_dump($_POST);
+        deleteGameFromDB($_POST['gameId']);
+    }
 ?>
