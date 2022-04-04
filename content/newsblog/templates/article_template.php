f5fb11b84368d836d8cdcdaac27039b502751675
dastrinken <61236723+dastrinken@users.noreply.github.com>
2022-04-04 21:19:49 +0200

    Kommentarfunktion und Header / Sidebar-fix
    Erste Schritte Kommentarfunktion, header und sidebar geändert, Rechtesystem erweitert.

--- content/newsblog/templates/article_template.php
+++ content/newsblog/templates/article_template.php
@@ -1,8 +1,10 @@
 <?php
+    $displayDate = date_create_from_format("Y-m-d H:i:s", $article_date_published)->format("d.m.Y");
+    $displayTime = date_create_from_format("Y-m-d H:i:s", $article_date_published)->format("H:i");
 ?>
 <link rel="stylesheet" href="./content/newsblog/newsblog_style.css">
-<div class="content mb-4 p-3 bg-lightdark">
-    <div class="border-bottom border-dark rounded mb-2 p-2 text-center" style="<?php echo "background: ".$article_color; ?>">
+<div class="border-black rounded mb-4 p-3 bg-lightdark">
+    <div class="border-bottom border-black shadow-sm rounded mb-2 p-2 text-center" style="<?php echo "background: ".$article_color; ?>">
         <h3><?php echo $article_headline; ?><!--<span class="badge bg-highlighted ml-2">New</span>--></h3>
     </div>
     <p>
@@ -10,11 +12,16 @@
     </p>
     <hr />
     <p>
-        Article footer - <?php echo "Author: ".$article_name_author." Published: ".$article_date_published; ?>, contact info, tags, categories, etc
+        <?php echo $article_name_author." - ".$displayDate." ".$displayTime; ?>
     </p>
-        <ul class="col-md-20 justify-content-end list-unstyled d-flex">
-            <li>
-                <a href="#" Title="Kommentare lesen" alt="Kommentare"><i class="bi-chat-text article-icon"></i></a> | <a href="#" Title="Kommentar schreiben" alt="Kommentar schreiben"><i class="bi-pencil-square article-icon"></i></a> | <a href="#" title="Beitrag löschen" alt="Beitrag löschen"><i class="bi-trash3-fill article-icon"></i></a> | <a href="#" title="anpinnen" alt="Beitrag anpinnen"><i class="bi-pin-angle article-icon"></i></a>
-             </li>
-        </ul>
+    <div class="row">
+        <div class="col d-flex justify-content-center">
+            <button id="<?php echo $article_id; ?>" onclick="showComments(this.id);" class="btn w-50 bg-blackened btn-darkmode-outline article-icon fs-6" title="Kommentare lesen" alt="Kommentare"  data-bs-toggle="collapse" data-bs-target="#<?php echo 'collapse'.$article_id; ?>" aria-expanded="false" aria-controls="<?php echo 'content'.$article_id; ?>">X Kommentare</button>
+        </div>
+    </div>
+</div>
+<div class="collapse" id="<?php echo 'collapse'.$article_id; ?>">
+    <div id="<?php echo 'content'.$article_id; ?>" class="card card-body mb-4 bg-blackened border-0">
+        <!-- Kommentare -->
+    </div>
 </div>
