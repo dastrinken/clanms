bcbd884fdda9041981a758d22b9142c9d660456f
Usurper321 <97109375+Usurper321@users.noreply.github.com>
2022-04-04 18:26:50 +0200

    about us seite
    1st try about us seite

---
+++ content/about_us/about_us.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col m-3 rounded border border-white p-0 ">
+        <img src="./content/about_us/pexels-yan-krukov-9072394.jpg" class="w-100 rounded " alt="logo">
+    </div>
+
+    <div class="col m-3 d-flex align-items-center"> 
+        <h3 class="text-center">eine wunderschöne geschischte über unsere Grupee.</h3> 
+    </div>
+</div>
+
+<div class="row">
+    <div class="col m-3 bg-lightdark p-4 pb-3 rounded">
+        <p>it was a dark and stormy night Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga architecto vel debitis recusandae animi tenetur ipsum nulla non hic. Consequatur rem odio, facere hic nam suscipit quo modi fugit eius? Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus fuga sit architecto? Eveniet, quas sunt? Sapiente temporibus sit deserunt nisi, voluptas quam ducimus, facilis commodi reprehenderit omnis quae quis at!</p>
+    </div>
+</div>
